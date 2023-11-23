43c9a8668f1dc16cf371c53dfbb8a01bbd01203a
youtlane <youtlanehanane@gmail.com>
2023-11-23 12:59:42 +0100

    fix: date of birth & the message submiting the form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,13 +34,22 @@ function launchModal() {
 // close form
 closeIcon.addEventListener('click', function () {
   // Masquer le formulaire
-  modalbg.style.display = "none";
+  modalbg.style.display = "none";  
+  
 });
+closeIcon.addEventListener('click', function () {
+  // Masquer le formulaire
+  modalok.style.display = "none";
+  
+});
+
+
 
 // close modal passed
 const mdpassedclose = document.getElementById('mdclose');
 mdpassedclose.addEventListener('click', function () {
   modalbg.style.display = "none";
+  
 });
 
 
@@ -83,79 +92,79 @@ function verifInput(condition, nameForm, errorMessage) {
 // }
 
 async function validateForm() {
-    console.log('o ');
-    let errorExist = false;
+  console.log('o ');
+  let errorExist = false;
 
-    // Validate First Name
-    const firstName = document.getElementById('first').value;
-    const firstNameForm = document.getElementById('firstNameForm');
-    const firstNameCondition = firstName.length < 2;
-    const firstNameError = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    if (await verifInput(firstNameCondition, firstNameForm, firstNameError)) {
-      errorExist = true;
-    }
+  // Validate First Name
+  const firstName = document.getElementById('first').value;
+  const firstNameForm = document.getElementById('firstNameForm');
+  const firstNameCondition = firstName.length < 2;
+  const firstNameError = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
+  if (await verifInput(firstNameCondition, firstNameForm, firstNameError)) {
+    errorExist = true;
+  }
 
-    // Validate Last Name
-    const lastName = document.getElementById('last').value;
-    const lastNameForm = document.getElementById('lastNameForm');
-    const lastNameCondition = lastName.length < 2;
-    const lastNameError = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-    if (await verifInput(lastNameCondition, lastNameForm, lastNameError)) {
-      errorExist = true;
-    }
+  // Validate Last Name
+  const lastName = document.getElementById('last').value;
+  const lastNameForm = document.getElementById('lastNameForm');
+  const lastNameCondition = lastName.length < 2;
+  const lastNameError = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+  if (await verifInput(lastNameCondition, lastNameForm, lastNameError)) {
+    errorExist = true;
+  }
 
-    // Validate Email
-    const email = document.getElementById('email').value;
-    const emailForm = document.getElementById('emailForm');
-    const emailCondition = !emailRegex.test(email);
-    const emailError = 'Email non valide.';
-    if (await verifInput(emailCondition, emailForm, emailError)) {
-      errorExist = true;
-    }
+  // Validate Email
+  const email = document.getElementById('email').value;
+  const emailForm = document.getElementById('emailForm');
+  const emailCondition = !emailRegex.test(email);
+  const emailError = 'Email non valide.';
+  if (await verifInput(emailCondition, emailForm, emailError)) {
+    errorExist = true;
+  }
 
-    // Validate birthdate
-    const birthdate = document.getElementById('birthdate').value;
-    const birthdateForm = document.getElementById('birthdateForm');
-    const birthdateCondition = !birthdate;
-    const birthdateError = 'Vous devez entrer votre date de naissance.';
-    if (await verifInput(birthdateCondition, birthdateForm, birthdateError)) {
-      errorExist = true;
-    }
+  // Validate birthdate
+  const birthdate = document.getElementById('birthdate').value;
+  const birthdateForm = document.getElementById('birthdateForm');
+  const birthdateCondition = !birthdate || new Date(birthdate) < new Date('1900-01-01') || new Date(birthdate) > new Date('2005-12-31');
+  const birthdateError = 'Vous devez entrer votre date de naissance.';
+  if (await verifInput(birthdateCondition, birthdateForm, birthdateError)) {
+    errorExist = true;
+  }
 
-    // Validate Quantity
-    const quantity = document.getElementById('quantity').value;
-    const quantityForm = document.getElementById('quantityForm');
-    const quantityCondition = quantity === '' || quantity.length < 0;
-    const quantityError = 'Veuillez entrer un numéro.';
+  // Validate Quantity
+  const quantity = document.getElementById('quantity').value;
+  const quantityForm = document.getElementById('quantityForm');
+  const quantityCondition = quantity === '' || quantity.length < 0;
+  const quantityError = 'Veuillez entrer un numéro.';
 
-    console.log();
-    if (await verifInput(quantityCondition, quantityForm, quantityError)) {
-      errorExist = true;
-    }
+  console.log();
+  if (await verifInput(quantityCondition, quantityForm, quantityError)) {
+    errorExist = true;
+  }
 
-    // Validate Location
-    const selectedLocation = document.querySelector('input[name="location"]:checked');
-    const selectedLocationForm = document.getElementById('selectedLocationForm');
-    const locationCondition = !selectedLocation;
-    const locationError = 'Vous devez choisir une option.';
-    if (await verifInput(locationCondition, selectedLocationForm, locationError)) {
-      errorExist = true;
-    }
+  // Validate Location
+  const selectedLocation = document.querySelector('input[name="location"]:checked');
+  const selectedLocationForm = document.getElementById('selectedLocationForm');
+  const locationCondition = !selectedLocation;
+  const locationError = 'Vous devez choisir une option.';
+  if (await verifInput(locationCondition, selectedLocationForm, locationError)) {
+    errorExist = true;
+  }
 
-    // Validate conditions générales
-    const checkbox1 = document.getElementById('checkbox1');
-    const checkbox1Form = document.getElementById('checkbox1Form');
-    const checkboxCondition = !checkbox1.checked;
-    const checkboxError = 'Vous devez vérifier que vous acceptez les termes et conditions.';
-    if (await verifInput(checkboxCondition, checkbox1Form, checkboxError)) {
-      errorExist = true;
-    }
+  // Validate conditions générales
+  const checkbox1 = document.getElementById('checkbox1');
+  const checkbox1Form = document.getElementById('checkbox1Form');
+  const checkboxCondition = !checkbox1.checked;
+  const checkboxError = 'Vous devez vérifier que vous acceptez les termes et conditions.';
+  if (await verifInput(checkboxCondition, checkbox1Form, checkboxError)) {
+    errorExist = true;
+  }
 
 
-    console.log('isValid ', errorExist);
+  console.log('isValid ', errorExist);
 
-    // Resolve with the result of the validation
-    return !errorExist;
+  // Resolve with the result of the validation
+  return !errorExist;
 }
 
 // function validate() {
@@ -172,7 +181,9 @@ async function validateForm() {
 // }
 
 async function validate() {
-    event.preventDefault(); // Empêcher le comportement de soumission par défaut du formulaire
+  event.preventDefault();
+
+  // Empêcher le comportement de soumission par défaut du formulaire
 
   try {
     const isValid = await validateForm();
@@ -180,7 +191,7 @@ async function validate() {
     if (isValid) {
       //document.forms["reserve"].submit();
       formsReser.style.display = "none";
-      modalok.style.display = "flex"; 
+      modalok.style.display = "flex";
     }
   } catch (error) {
     console.error('Une erreur est survenue lors de la validation:', error);
